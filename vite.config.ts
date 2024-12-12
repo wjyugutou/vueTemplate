648bd65490500c0dc6bde998968dd8298f3cf42c
WJ <1220253798@qq.com>
2024-12-12 15:01:55 +0800

    feat: 开启lightningcss 处理css

--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,61 @@
 import path from 'node:path'
 import Vue from '@vitejs/plugin-vue'
+import browserslist from 'browserslist'
+import { browserslistToTargets } from 'lightningcss'
 import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
 import VueRouter from 'unplugin-vue-router/vite'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig({
-  envDir: './env',
-  resolve: {
-    alias: {
-      '@/': `${path.resolve(__dirname, 'src')}/`,
+export default defineConfig(({ mode }) => {
+  // const env = loadEnv(mode, './env') as ImportMetaEnv
+
+  return {
+    envDir: './env',
+    resolve: {
+      alias: {
+        '@/': `${path.resolve(__dirname, 'src')}/`,
+      },
     },
-  },
-  plugins: [
-    Vue(),
-    // https://github.com/antfu/unocss
-    // see unocss.config.ts for config
-    UnoCSS(),
-    // https://uvr.esm.is/
-    VueRouter({
-      exclude: ['src/pages/auth/**'],
-      dts: './types/vue-router.d.ts',
-    }),
-    // https://github.com/antfu/unplugin-auto-import
-    AutoImport({
-      imports: [
-        'vue',
-        '@vueuse/core',
-        VueRouterAutoImports,
-      ],
-      dts: './types/auto-imports.d.ts',
-      dirs: [
-        './src/composables',
-      ],
-    }),
+    plugins: [
+      Vue(),
+      // https://github.com/antfu/unocss
+      // see unocss.config.ts for config
+      UnoCSS(),
+      // https://uvr.esm.is/
+      VueRouter({
+        exclude: ['src/pages/auth/**'],
+        dts: './types/vue-router.d.ts',
+      }),
+      // https://github.com/antfu/unplugin-auto-import
+      AutoImport({
+        imports: [
+          'vue',
+          '@vueuse/core',
+          VueRouterAutoImports,
+        ],
+        dts: './types/auto-imports.d.ts',
+        dirs: [
+          './src/composables',
+        ],
+      }),
 
-    // https://github.com/antfu/vite-plugin-components
-    Components({
-      dts: './types/components.d.ts',
-    }),
+      // https://github.com/antfu/vite-plugin-components
+      Components({
+        dts: './types/components.d.ts',
+      }),
 
-  ],
+    ],
+    build: {
+      cssMinify: 'lightningcss',
+    },
+    css: {
+      transformer: 'lightningcss',
+      lightningcss: {
+        targets: browserslistToTargets(browserslist()),
+      },
+    },
+  }
 })
